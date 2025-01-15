375711bc0770f1fb094ff94db0fca0cc8eefe82d
Aziul <y.nazar9@gmail.com>
2025-01-15 21:27:48 +0200

    extract to functions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import { useAssets } from '@/hooks/useAssets';
 import { useColumns } from '@/hooks/useColums';
 import { Image } from 'expo-image';
 import * as MediaLibrary from 'expo-media-library';
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { FlatList, SectionList, StyleSheet, Text, View } from "react-native";
 
 const PLACEHOLDER = require('@/assets/images/placeholder-rect.png')
@@ -31,6 +31,45 @@ export default function PhotosScreen() {
     ).map(([day, photos]) => ({ title: day, data: [{ list: photos }] }))
   }, [assets])
 
+  const renderFlatListImage = useCallback(({ item: photo, index }: { item: MediaLibrary.Asset, index: number }) => (
+    <Image
+      key={index + photo.id + photo.uri}
+      placeholder={PLACEHOLDER}
+      placeholderContentFit='contain'
+      style={{ width, height }}
+      source={{
+        uri: photo.uri,
+      }}
+    />
+  ), [width, height])
+  const flatListKeyExtractor = (item: MediaLibrary.Asset) => item.id
+  const renderSectionList = useCallback(({ item }: { item: { list: MediaLibrary.Asset[] } }) => (
+    <FlatList
+      removeClippedSubviews
+      numColumns={totalColumns}
+      columnWrapperStyle={{
+        gap: 1
+      }}
+      key={totalColumns}
+      data={item.list}
+      renderItem={renderFlatListImage}
+      keyExtractor={(item) => item.id}
+      ItemSeparatorComponent={() => <View style={{ height: 1 }} />}
+      getItemLayout={(_, index) => (
+        { length: height + 1, offset: index * (height + 1), index }
+      )}
+    />
+
+  ), [totalColumns, renderFlatListImage, height])
+  const sectionListKeyExtractor = (item: { list: MediaLibrary.Asset[] }, id: number) => id + item.list[0].id
+
+  const renderSectionHeader = useCallback(
+    ({ section: { title } }: { section: { title: string } }) => (
+      <Text style={{ color: '#eee', fontSize: 16 }}>{title}</Text>
+    ),
+    [],
+  );
+
   return (
     <View style={styles.container}>
       <SectionList
@@ -45,44 +84,10 @@ export default function PhotosScreen() {
         onEndReached={loadMoreAssets}
         onEndReachedThreshold={0.5}
         sections={groupByDate}
-        keyExtractor={(item, id) => id + item.list[0].id}
-        renderItem={({ item }) => (
-
-          <FlatList
-            removeClippedSubviews
-            numColumns={totalColumns}
-            columnWrapperStyle={{
-              gap: 1
-            }}
-            key={totalColumns}
-            data={item.list}
-            renderItem={({ item: photo, index }) => (
-              <Image
-                key={index + photo.id + photo.uri}
-                placeholder={PLACEHOLDER}
-                placeholderContentFit='contain'
-                style={{ width, height }}
-                source={{
-                  uri: photo.uri,
-                }}
-              />
-            )}
-            keyExtractor={(item) => item.id}
-            ItemSeparatorComponent={() => <View style={{ height: 1 }} />}
-            getItemLayout={(_, index) => (
-              { length: height, offset: height * index, index }
-            )}
-          />
-
-        )}
-        renderSectionHeader={({ section: { title } }) => (
-          <Text style={{
-            color: '#eee',
-            fontSize: 16,
-          }} >{title}</Text>
-        )}
+        keyExtractor={sectionListKeyExtractor}
+        renderItem={renderSectionList}
+        renderSectionHeader={renderSectionHeader}
       />
-
     </View>
   )
 
